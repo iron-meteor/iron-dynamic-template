a839659cb0012d2220217c50492208cd0009f812
Tom Coleman <tom@thesnail.org>
2014-07-23 08:31:17 +1000

    Added a failing test for parent data inheritance.

--- dynamic_template_test.js
+++ dynamic_template_test.js
@@ -197,6 +197,12 @@ Tinytest.add('DynamicTemplate - Rendering with dynamic parent data', function (t
   });
 });
 
+Tinytest.add('DynamicTemplate - Rendering inherits data correctly', function (test) {
+  withRenderedTemplate('InheritedParentData', function (el) {
+    test.equal(el.innerHTML.compact(), 'WithDataAndParentData-inner-outer');
+  });
+});
+
 
 Tinytest.add('DynamicTemplate - Block content', function (test) {
   withRenderedTemplate('DynamicWithBlock', function (el) {
